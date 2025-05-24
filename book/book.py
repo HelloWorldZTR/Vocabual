142e66b0997b1758f38607175270e0af3226965d
Jiu-He25 <1582764900@qq.com>
2025-05-24 22:25:24 +0800

    初始化book数据

--- book/book.py
+++ book/book.py
@@ -1,14 +1,23 @@
 """
 处理书籍相关操作
 """
+import pandas as pd
+import os
 
+def get_root_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+book = pd.read_csv((get_root_dir()+'\\DictionaryData\\book.csv'), sep=">")
+word = pd.read_csv((get_root_dir()+'\\DictionaryData\\word.csv'), sep=">")
+relation_book_word = pd.read_csv((get_root_dir()+'\\DictionaryData\\relation_book_word.csv'), sep=">")
+word_translation = pd.read_csv((get_root_dir()+'\\DictionaryData\\word_translation.csv'))
 
 class Book:
-    def __init__(self, title, cover, word_count, word_list_db_table):
+    def __init__(self, id, title, word_count):
+        self.id = id
         self.title = title
-        self.cover = cover
         self.word_count = word_count
-        self.word_list_db_table = word_list_db_table
+        self.relation_book_word_id_list = []
         self.word_list = []
     def load_from_db(self):
         """
