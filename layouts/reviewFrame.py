79211ded2f2c731cf025a2a475a7b01e6d085d19
Jiu-He25 <1582764900@qq.com>
2025-05-25 22:24:43 +0800

    feat: 优化复习界面逻辑，改进按钮反馈和随机单词选择

--- layouts/reviewFrame.py
+++ layouts/reviewFrame.py
@@ -1,7 +1,6 @@
 from qfluentwidgets import Dialog
 from utils.uitools import FrameWrapper
 from .Ui_review import Ui_Frame 
-from extern import webDict
 from PyQt5.QtCore import QTimer
 import settings
 import random
@@ -10,6 +9,7 @@ import bookdata
 class ReviewFrame(FrameWrapper):
     """ 背单词界面初始化 """
     def __init__(self, parent=None, unique_name=None):
+        super().__init__(Ui_Frame(), parent=parent, unique_name=unique_name)
         self.root = parent
         self.wordList = settings.get_favourite_list()
         if len(self.wordList) == 0:
@@ -25,14 +25,17 @@ class ReviewFrame(FrameWrapper):
         self.stage=random.randint(0, 1)
         self.answer=random.randint(1,4)
         self.buttons = [None, self.frame.A, self.frame.B, self.frame.C, self.frame.D]
-        super().__init__(Ui_Frame(), parent=parent, unique_name=unique_name)
         if self.stage == 0:
             self.frame.wordLabel_2.setText(self.currentWord)
             for i in range(1, 5):
                 if i == self.answer:
                     self.buttons[i].setText(self.currentTranslation)
                 else:
-                    self.buttons[i].setText((random.choice(self.otherWordList)).translation)
+                    while True:
+                        randomWord = random.choice(self.otherWordList)
+                        if randomWord.translation != self.currentTranslation and randomWord.translation != "暂无翻译":
+                            break
+                    self.buttons[i].setText(randomWord.translation)
         else:
             self.frame.wordLabel_2.setText(self.currentTranslation)
             for i in range(1, 5):
@@ -46,7 +49,9 @@ class ReviewFrame(FrameWrapper):
         self.currentWordIndex += 1
         if self.currentWordIndex >= len(self.wordList):
             self.logEvent('已完成')
-            self.frame.wordLabel_2.setText('😀')
+            self.frame.wordLabel_2.setText('今日复习任务已经完成啦！😀')
+            for i in range(1, 5):
+                self.buttons[i].hide()
         else:
             self.currentWord = self.wordList[self.currentWordIndex].word
             self.currentTranslation = self.wordList[self.currentWordIndex].translation
@@ -58,7 +63,11 @@ class ReviewFrame(FrameWrapper):
                     if i == self.answer:
                         self.buttons[i].setText(self.currentTranslation)
                     else:
-                        self.buttons[i].setText((random.choice(self.otherWordList)).translation)
+                        while True:
+                            randomWord = random.choice(self.otherWordList)
+                            if randomWord.translation != self.currentTranslation and randomWord.translation != "暂无翻译":
+                                break
+                        self.buttons[i].setText(randomWord.translation)
             else:
                 self.frame.wordLabel_2.setText(self.currentTranslation)
                 for i in range(1, 5):
@@ -68,101 +77,30 @@ class ReviewFrame(FrameWrapper):
                         self.buttons[i].setText((random.choice(self.otherWordList)).word)
     """还原按钮"""
     def resetButton(self):
-        """还原按钮颜色"""
-        self.frame.A.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.frame.B.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.frame.C.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.frame.D.setStyleSheet("background-color: rgb(255, 255, 255);")
+        for i in range(1, 5):
+            self.buttons[i].setStyleSheet("color: black;")
     """不同按钮"""
-    def A(self):
-        if self.answer == 1:
-            self.frame.A.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 2:
-            self.frame.A.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.B.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 3:
-            self.frame.A.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.C.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 4:
-            self.frame.A.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.D.setStyleSheet("background-color: rgb(0, 255, 0);")
-        #QTimer.singleShot(5000)
+    def onClickButton(self, id:int):
+        if id == self.answer:
+            self.buttons[id].setText( self.buttons[id].text() + " ✔")
+            self.buttons[id].setStyleSheet("color: green;")
+        else:
+            self.buttons[id].setText( self.buttons[id].text() + " ✘")
+            self.buttons[self.answer].setText( self.buttons[self.answer].text() + " ✔")
+            self.buttons[id].setStyleSheet("color: red;")
+            self.buttons[self.answer].setStyleSheet("color: green;")
         QTimer.singleShot(5000, self.nextWord)
-        
-    def B(self):
-        if self.answer == 1:
-            self.frame.B.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 2:
-            self.frame.B.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.A.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 3:
-            self.frame.B.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.D.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 4:
-            self.frame.B.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.C.setStyleSheet("background-color: rgb(0, 255, 0);")
-        #QTimer.singleShot(5000)
-        QTimer.singleShot(5000, self.nextWord)
-    def C(self):
-        if self.answer == 1:
-            self.frame.C.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 2:
-            self.frame.C.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.D.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 3:
-            self.frame.C.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.A.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 4:
-            self.frame.C.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.B.setStyleSheet("background-color: rgb(0, 255, 0);")
-        #QTimer.singleShot(5000)
-        QTimer.singleShot(5000, self.nextWord)
-    def D(self):
-        if self.answer == 1:
-            self.frame.D.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 2:
-            self.frame.D.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.C.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 3:
-            self.frame.D.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.B.setStyleSheet("background-color: rgb(0, 255, 0);")
-        elif self.answer == 4:
-            self.frame.D.setStyleSheet("background-color: rgb(255, 0, 0);")
-            self.frame.A.setStyleSheet("background-color: rgb(0, 255, 0);")
-        #QTimer.singleShot(5000)
-        QTimer.singleShot(5000, self.nextWord)
+    
         
     """连接信号和槽函数"""
     def setupConnections(self):
-        self.frame.A.clicked.connect(self.A)
-        self.frame.B.clicked.connect(self.B)
-        self.frame.C.clicked.connect(self.C)
-        self.frame.D.clicked.connect(self.D)
+        self.frame.A.clicked.connect(lambda: self.onClickButton(1))
+        self.frame.B.clicked.connect(lambda: self.onClickButton(2))
+        self.frame.C.clicked.connect(lambda: self.onClickButton(3))
+        self.frame.D.clicked.connect(lambda: self.onClickButton(4))
 
     def logEvent(self, text):
         print("[log] " + text)
-    
-    def addToFavourite(self):
-        word = self.wordList[self.currentWordIndex]
-        self.logEvent('添加到收藏夹'+word.word)
-        settings.add_favourite(word.id)
-        self.frame.favouriteButton.checked = True
-
-    def pronounce(self, type:int):
-        """
-        发音按钮点击事件
-        type: 0 - 英式发音, 1 - 美式发音
-        """
-        if type == 0:
-            self.logEvent('英式发音按钮被点击')
-        elif type == 1:
-            self.logEvent('美式发音按钮被点击')
-        
-        bytes = webDict.query_spelling(self.currentWord, type)
-        if bytes:
-            self.player.play_raw(bytes)
-        else:
-            self.logEvent('发音查询失败')
         
     def updateWindow(self):
         pass
