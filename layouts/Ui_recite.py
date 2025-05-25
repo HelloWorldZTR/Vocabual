458a0956c300a1433cdc01574b307729f82615f1
Shy524619 <2042438052@qq.com>
2025-05-25 15:02:22 +0800

    添加

--- layouts/Ui_recite.py
+++ layouts/Ui_recite.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\Develop\Vocabual\layouts\recite.ui'
+# Form implementation generated from reading ui file 'e:\VSwork\helloworld\Vocabual\layouts\recite.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
