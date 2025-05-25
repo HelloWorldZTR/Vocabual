bc84666fb042206493d9acae67772e1e13201b6a
Andy Zhang <2279429006@qq.com>
2025-05-25 16:16:47 +0800

    add bookshelf

--- layouts/bookshelfFrame.py
+++ layouts/bookshelfFrame.py
@@ -1,14 +1,56 @@
 from utils.uitools import FrameWrapper
-from .Ui_bookshelf import Ui_Frame
-from qfluentwidgets import GroupHeaderCardWidget, PushButton, ComboBox, SearchLineEdit, IconWidget, InfoBarIcon, BodyLabel, PrimaryPushButton
-from qfluentwidgets import FluentIcon
-from PyQt5.QtWidgets import QHBoxLayout
+from qfluentwidgets import (
+    GroupHeaderCardWidget,
+    PushButton,
+    ComboBox,
+    SearchLineEdit,
+    IconWidget,
+    InfoBarIcon,
+    BodyLabel,
+    PrimaryPushButton,
+    SimpleCardWidget,
+    TitleLabel,
+    ImageLabel
+)
+from qfluentwidgets import FluentIcon, TreeWidget
+
+from PyQt5.QtWidgets import QHBoxLayout, QFrame, QVBoxLayout, QSizePolicy, QSpacerItem, QTreeWidgetItem
 from PyQt5.QtCore import Qt
 
-class BookshelfFrame(FrameWrapper):
+import bookdata
+
+class PrevirewCard(SimpleCardWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.setLayout(QVBoxLayout())
+        self.setMinimumHeight(200)
+        self.setMinimumWidth(200)
+        self.setObjectName("previewCard")
+
+        self.bookTitle = TitleLabel("书名", self)
+        self.bookImg = ImageLabel(self)
+        self.infoLabel = BodyLabel("书籍简介", self)
+        self.infoLabel.setWordWrap(True)
+
+        self.layout().addWidget(self.bookTitle)
+        self.layout().addWidget(self.bookImg)
+        self.layout().addWidget(self.infoLabel)
+
+class BookshelfFrame(QFrame):
     def __init__(self, parent=None, unique_name=None):
-        super().__init__(Ui_Frame(), parent, unique_name)
+        super().__init__(parent)
+        self.setObjectName(unique_name)
 
-    def setupConnections(self):
-        pass
+        self.setLayout(QHBoxLayout())
 
+        self.treeview = TreeWidget(self)
+        self.layout().addWidget(self.treeview)
+        self.preview = PrevirewCard(self)
+        self.layout().addWidget(self.preview)
+        self.treeview.setHeaderHidden(True)
+        self.treeview.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
+
+        self.treedata = bookdata.get_book_tree()
+        print(self.treedata)
