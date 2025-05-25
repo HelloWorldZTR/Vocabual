91196ea5ef387cbc643a34aa2db1c1eef146dfd8
Andy Zhang <2279429006@qq.com>
2025-05-25 16:50:45 +0800

    feat: Enhance bookshelf functionality with book selection and preview
    - Added a new button to select the book in the preview card.
    - Implemented item click handling in the tree view to update the preview card with book details.
    - Integrated book selection functionality to store the selected book ID in settings.
    - Updated the settings JSON to include a new entry for the selected book ID.

--- layouts/bookshelfFrame.py
+++ layouts/bookshelfFrame.py
@@ -10,7 +10,8 @@ from qfluentwidgets import (
     PrimaryPushButton,
     SimpleCardWidget,
     TitleLabel,
-    ImageLabel
+    ImageLabel,
+    PillPushButton
 )
 from qfluentwidgets import FluentIcon, TreeWidget
 
@@ -18,6 +19,7 @@ from PyQt5.QtWidgets import QHBoxLayout, QFrame, QVBoxLayout, QSizePolicy, QSpac
 from PyQt5.QtCore import Qt
 
 import bookdata
+import settings
 
 class PrevirewCard(SimpleCardWidget):
     def __init__(self, parent=None):
@@ -29,13 +31,14 @@ class PrevirewCard(SimpleCardWidget):
         self.setObjectName("previewCard")
 
         self.bookTitle = TitleLabel("书名", self)
-        self.bookImg = ImageLabel(self)
         self.infoLabel = BodyLabel("书籍简介", self)
-        self.infoLabel.setWordWrap(True)
+        self.infoLabel.setText("请从左侧选择一本书籍以查看详细信息。")
+        self.infoLabel.setAlignment(Qt.AlignLeft)
+        self.selectBtn = PrimaryPushButton("选择该书", self)
 
         self.layout().addWidget(self.bookTitle)
-        self.layout().addWidget(self.bookImg)
         self.layout().addWidget(self.infoLabel)
+        self.layout().addWidget(self.selectBtn)
 
 class BookshelfFrame(QFrame):
     def __init__(self, parent=None, unique_name=None):
@@ -50,7 +53,43 @@ class BookshelfFrame(QFrame):
         self.layout().addWidget(self.preview)
         self.treeview.setHeaderHidden(True)
         self.treeview.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.selected_book_id = None
         
 
         self.treedata = bookdata.get_book_tree()
-        print(self.treedata)
+        for item in self.treedata:
+            book_item = QTreeWidgetItem(self.treeview)
+            book_item.setText(0, self.treedata[item]['title'])
+            book_item.setData(0, Qt.UserRole, item)
+            for child in self.treedata[item]['children']:
+                child_item = QTreeWidgetItem(book_item)
+                child_item.setText(0, self.treedata[item]['children'][child]['title'])
+                child_item.setData(0, Qt.UserRole, child)
+                # 这里可以添加更多的子节点或属性
+                book_item.addChild(child_item)
+        
+        self.treeview.expandAll()
+        self.treeview.itemClicked.connect(self.on_item_clicked)
+        self.preview.selectBtn.clicked.connect(self.select_book)
+        # print(self.treedata)
+    
+    def on_item_clicked(self, item, column):
+        """
+        当树形视图中的项目被点击时，更新预览卡片的内容。
+        :param item: 被点击的树形项目
+        :param column: 被点击的列
+        """
+        book_id = item.data(0, Qt.UserRole)
+        book = bookdata.books[book_id]
+        
+        self.preview.bookTitle.setText(book.title)
+        self.preview.infoLabel.setText(
+"""书籍ID: {book_id}\n
+单词量: {word_count}\n""".format(book_id=book_id, word_count=book.word_count)
+        )
+        self.selected_book_id = book_id
+        print("[log] Select" + book.title)
+    
+    def select_book(self):
+        print("[log] Selected book ID:", self.selected_book_id)
+        settings.set_book_id(self.selected_book_id)
