53bee13a037b2f5ad256f2255600dacf483552ee
Andy Zhang <2279429006@qq.com>
2025-05-29 13:14:09 +0800

    Fix: correct file path for layout assets in pack.cmd

--- pack.cmd
+++ pack.cmd
@@ -1,4 +1,4 @@
-pyinstaller --add-data "bookdata;bookdata"  --add-data "settings;settings" --add-data "layouts/favico.png;layouts/favico.png" --icon "favico.ico" --collect-data pyecharts --noconsole .\main.py
+pyinstaller --add-data "bookdata;bookdata"  --add-data "settings;settings" --add-data "layouts;layouts" --icon "favico.ico" --collect-data pyecharts --noconsole .\main.py
 if %errorlevel% neq 0 (
     echo "PyInstaller failed to build the executable."
     exit /b %errorlevel%
